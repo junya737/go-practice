1bd9014c9151918c88ff2948594bc31ecc0972fe
ihiratch <junya737@gmail.com>
2025-01-09 01:02:21 +0900

    function

--- helloworld.go
+++ helloworld.go
@@ -6,4 +6,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+
+	// 変数の宣言
+	var x int = 1
+	var y int = 2
+	fmt.Println(add(x, y))
+
+}
+
+// 足し算をする関数
+func add(x int, y int) int {
+	return x + y
 }
